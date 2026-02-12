f51ddd8482c0cb96657d1ed5e77b3df35fc71c88
crypt-soil <LOIS.24@ichat.sp.edu.sg>
2026-02-12 22:41:45 +0800

    removed irrelevant files

--- companion-portal/src/main/java/com/silvercare/companion_portal/model/partner/PartnerInfo.java
+++
@@ -1,8 +0,0 @@
-package com.silvercare.companion_portal.model.partner;
-
-public class PartnerInfo {
-    public int partnerId;
-    public String username;
-    public String email;
-    public String companyName;
-}
